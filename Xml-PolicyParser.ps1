92a2914d196e93612e729d995fd9f60a65ac3948
rudyrebolledo <rudyrebolledo68@gmail.com>
2017-09-21 11:41:41 -0500

    Add files via upload

---
+++ Xml-PolicyParser.ps1
@@ -0,0 +1,23 @@
+﻿$XmlDocument = [Xml](Get-Content "realm-policies-short3.xml")
+
+foreach ( $Policy in $XmlDocument.PolicySet.Policy) {
+
+    # Load Root Template Xml
+    $RootPolicyXML = [Xml](Get-Content "Xml-PolicyRoot.xml")
+
+    $RootPolicyXML.PolicySet.AppendChild($RootPolicyXML.ImportNode($Policy, $true))
+
+    $CurrentPath = Convert-Path .
+
+    # Remove special characters
+    $RegExPattern = '[^a-zA-Z]'
+    $FileNameClean = $Policy.GetAttribute("PolicyId") -replace $RegExPattern, "_"
+
+    $FilePath = Join-Path $CurrentPath $FileNameClean
+
+    Write-Output ($FilePath + ".xml")
+    
+    $RootPolicyXML.OuterXml | Out-File -FilePath ($FilePath + ".xml")
+
+}
+
